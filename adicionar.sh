84fbba2482327c04e7330fdc32ebd0adb4208c41
faelsfernandes <faelsfernandes@gmail.com>
2017-12-24 13:06:57 -0200

    Teste

--- adicionar.sh
+++ adicionar.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-echo "Adicionando arquivos..."
-if ! git add $1
-then 
-	echo "Erro ao adicionar atualizar"
-	exit 1
-fi
-git commit -am "Exerc’cio Resolvido"
-git pull
-faelsfernandes
-Fernandes5672
-echo "Adicionado com sucesso!"
-
-exit 1
+teste
