d7fa6b4a200163d78c58f0f4a48374f33529a06a
faelsfernandes <faelsfernandes@gmail.com>
2017-12-26 20:43:22 -0200

    Exercício Resolvido

---
+++ 1024.py
@@ -0,0 +1,13 @@
+texto = input()
+for i in range(len(texto)):
+	texto = list(texto)
+	if (ord(texto[i]) >= 65 and ord(texto[i]) <= 90) or (ord(texto[i]) >= 97 and ord(texto[i]) <= 122):
+		texto[i] = chr(ord(texto[i])+3)
+
+if len(texto) > 1:
+	texto.reverse()
+	
+for i in range(int(len(texto)/2), len(texto)):
+			texto[i] = chr(ord(texto[i])-1)	
+texto = "".join(texto)
+print(texto)
