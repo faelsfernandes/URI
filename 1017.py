20de66f78acba751b5783990bdf5e9f7cd60bbeb
faelsfernandes <faelsfernandes@gmail.com>
2017-12-25 22:54:45 -0200

    Exercício Resolvido

--- 1017.py
+++ 1017.py
@@ -1,1 +1,4 @@
-teste2asdasd
+tempo = int(input())
+velocidade = int(input())
+
+print("%0.3f" % (float((tempo*velocidade)/12)))
---
+++ 1019.py
@@ -0,0 +1,12 @@
+segundos = int(input())
+auxsegundos = segundos
+
+horas = minutos = 0
+
+horas = int(segundos/3600)
+segundos -= horas*3600
+
+minutos = int(segundos/60)
+segundos -= minutos*60
+	
+print("%d:%d:%d" %(horas, minutos, segundos))
